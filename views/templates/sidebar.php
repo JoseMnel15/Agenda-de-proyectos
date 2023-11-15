2692edae7f96d77b1bb7a3d44e7c363ed57420ac
Jose Manuel Medina <medinajosemanuel511@gmail.com>
2023-11-15 11:00:27 -0600

    Se agrego la seccion de calendario

--- views/templates/sidebar.php
+++ views/templates/sidebar.php
@@ -12,6 +12,7 @@
         <a class="<?php echo ($titulo === 'Proyectos') ? 'activo' : ''; ?>" href="/dashboard">Proyectos</a>
         <a class="<?php echo ($titulo === 'Crear Proyecto') ? 'activo' : ''; ?>" href="/crear-proyecto">Crear Proyecto</a>
         <a class="<?php echo ($titulo === 'Perfil') ? 'activo' : ''; ?>" href="/perfil">Perfil</a>
+        <a class="<?php echo ($titulo === 'Calendario') ? 'activo' : ''; ?>" href="#">Calendario</a>
     </nav>
 
     <div class="cerrar-sesion-mobile">
